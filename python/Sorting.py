d209e6267e30d496708b0e299a69f51d89437481
davidfliu <rumalzliu@gmail.com>
2016-08-18 08:41:57 -0400

    added quick sort

--- python/Sorting.py
+++ python/Sorting.py
@@ -7,7 +7,7 @@ Insertion Sort:
 	- Total # of elements is the same in new and old list.
 """
 
-def insertion_sort(aList):
+def insertionSort(aList):
 
 	for i in range(1, len(aList)):
 		val = aList[i]
@@ -23,13 +23,69 @@ Merge sort:
 	- Recursive calls subdivide lists down to size 1 (which is already sorted).
 """
 
-def merge_sort() 
+def mergeSort(alist):
+    print("Splitting ",alist)
+    if len(alist)>1:
+        mid = len(alist)//2
+        lefthalf = alist[:mid]
+        righthalf = alist[mid:]
+
+        mergeSort(lefthalf)
+        mergeSort(righthalf)
+
+        i=0
+        j=0
+        k=0
+        while i < len(lefthalf) and j < len(righthalf):
+            if lefthalf[i] < righthalf[j]:
+                alist[k]=lefthalf[i]
+                i=i+1
+            else:
+                alist[k]=righthalf[j]
+                j=j+1
+            k=k+1
+
+        while i < len(lefthalf):
+            alist[k]=lefthalf[i]
+            i=i+1
+            k=k+1
+
+        while j < len(righthalf):
+            alist[k]=righthalf[j]
+            j=j+1
+            k=k+1
+    print("Merging ",alist)
+
+"""
+Quick Sort:
+	- Selects a pivot value from list. 
+	- Creates two list, one with elements less than pivot and greater than.
+	- Sorts two lists and joins them with pivot.
+"""
+def quickSort(aList):
+	if len(aList) > 1:
+		pivot_index = len(aList)/2
+		smaller_list = []
+		bigger_list = []
+
+		for i, val in enumerate(aList):
+			if i != pivot_index:
+				if val < aList[pivot_index]:
+					smaller_list.append(val)
+				else:
+					bigger_list.append(val)
+
+		quickSort(smaller_list)
+		quickSort(bigger_list)
+		aList[:] = smaller_list + [aList[pivot_index]] + bigger_list
 
 
 def main():
 	items = [3, 2, 1, 6, 5]
-	print "Before sorting: ", items
-	insertion_sort(items)
-	print "After sorting:  ", items
+	print "Before sort: ", items
+	quickSort(items)
+	print "After sort:  ", items
+
+
 
 main()
